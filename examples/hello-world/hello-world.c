37fa49438afc247ec4e1701164a35f7cb6650290
BrendonLau <brendonlcy@hotmail.com>
2024-03-07 14:23:40 +0800

    init: assignment1

--- examples/hello-world/hello-world.c
+++ examples/hello-world/hello-world.c
@@ -54,7 +54,12 @@ PROCESS_THREAD(hello_world_process, ev, data)
   etimer_set(&timer, CLOCK_SECOND * 10);
 
   while(1) {
-    printf("Hello, world\n");
+    printf("\n");
+    printf("Brendon Lau Cheng Yang\n");
+    printf("Pang Yuan Ker\n");
+    printf("Ng Jun Kang\n");
+    printf("Yusuf Bin Musa\n");
+
 
     /* Wait for the periodic timer to expire and then restart the timer. */
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
