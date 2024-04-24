f73c364a8ad409a0fe2353548710aab1c949b5ad
Yusuf Bin Musa <yusuf.musa3@gmail.com>
2024-04-24 10:55:54 +0800

    chore: format transmitter code

--- examples/source_code/task2transmitter.c
+++ examples/source_code/task2transmitter.c
@@ -1,30 +1,29 @@
-#include "contiki.h"
-#include "sys/rtimer.h"
-#include "sys/etimer.h"
 #include "board-peripherals.h"
+#include "contiki.h"
+#include "lib/random.h"
+#include "net/linkaddr.h"
 #include "net/netstack.h"
 #include "net/nullnet/nullnet.h"
 #include "net/packetbuf.h"
-#include "lib/random.h"
-#include "net/linkaddr.h"
-#include <string.h>
-#include <stdio.h>
-#include <math.h>
 #include "node-id.h"
+#include "sys/etimer.h"
+#include "sys/rtimer.h"
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
 
 // From nbr.c
-#define WAKE_TIME RTIMER_SECOND/10
+#define WAKE_TIME RTIMER_SECOND / 10
 #define SLEEP_CYCLE 9
-#define SLEEP_SLOT RTIMER_SECOND/10
+#define SLEEP_SLOT RTIMER_SECOND / 10
 #define NUM_SEND 2
 
-#define LINK_THRESHOLD -66
-#define READING_LENGTH 10
-#define LIGHT_DEFAULT -1
-
-#define PROXIMITY_CONTACT_TIME (RTIMER_SECOND * 15)
+// For transmitter
 #define SENDER_TYPE 0
-#define DETECT_INTERVAL 15
+#define RSSI_THRESHOLD -69
+#define LIGHT_DEFAULT -1
+#define LIGHT_READING_LEN 10
 
 linkaddr_t dest_addr;
 
@@ -33,14 +32,14 @@ typedef struct {
   unsigned long src_id;
   unsigned long timestamp;
   unsigned long seq;
-  int device_type;
+  int device_type; // 0 for sender, 1 for receiver
 } data_packet_struct;
 
 typedef struct {
   unsigned long src_id;
   unsigned long timestamp;
   unsigned long seq;
-  int light_readings[READING_LENGTH];
+  int light_readings[LIGHT_READING_LEN];
 } data_light_packet_struct;
 
 static struct etimer interval_timer;
@@ -53,46 +52,42 @@ unsigned long curr_timestamp;
 static bool has_detected = false;
 static bool has_good_link = false;
 
-static int light_readings[READING_LENGTH] = {0};
+static int light_readings[LIGHT_READING_LEN] = {0};
 static int light_index = 0;
 
-
-PROCESS(nbr_discovery_process, "cc2650 neighbour discovery process");
-PROCESS(light_reading_process, "Light Reading Process");
-AUTOSTART_PROCESSES(&nbr_discovery_process, &light_reading_process);
-
+PROCESS(light_reading_process, "light reading process");
+PROCESS(nbr_discovery_process, "neighbour discovery process");
+AUTOSTART_PROCESSES(&light_reading_process, &nbr_discovery_process);
 
 static void init_opt_reading(void);
 static void get_light_reading(void);
 
-/**
-* LIGHT READING FROM ASSIGNMENT 2
-*/
+/**** START OF LIGHT READING FROM ASSIGNMENT 2 ***/
 static void get_light_reading() {
   int value;
   value = opt_3001_sensor.value(0);
 
-  if(value != CC26XX_SENSOR_READING_ERROR) {
-    if (light_index >= READING_LENGTH) {
+  if (value != CC26XX_SENSOR_READING_ERROR) {
+    // Skip updating light reading if the buffer is full
+    if (light_index >= LIGHT_READING_LEN) {
       // printf("skip updating light reading\n");
       return;
     }
 
     // printf("light sense[%d]: %d\n", light_index, value);
-    light_readings[light_index % READING_LENGTH] = value;
+    light_readings[light_index % LIGHT_READING_LEN] = value;
     light_index++;
   }
   init_opt_reading();
 }
 
-static void init_opt_reading(void) {
-  SENSORS_ACTIVATE(opt_3001_sensor);
-}
+static void init_opt_reading(void) { SENSORS_ACTIVATE(opt_3001_sensor); }
 
 PROCESS_THREAD(light_reading_process, ev, data) {
   PROCESS_BEGIN();
 
-  for (int i = 0; i < READING_LENGTH; i++) {
+  // Set the reading to default value, receiver will ignore the default value
+  for (int i = 0; i < LIGHT_READING_LEN; i++) {
     light_readings[i] = LIGHT_DEFAULT;
   }
   etimer_set(&interval_timer, CLOCK_SECOND);
@@ -106,13 +101,11 @@ PROCESS_THREAD(light_reading_process, ev, data) {
   PROCESS_END();
 }
 
-/**
-* END OF LIGHT READING FROM ASSIGNMENT 2
-*/
+/**** END OF LIGHT READING FROM ASSIGNMENT 2 ****/
 
-
-// Modified from nbr.c
-void receive_packet_callback(const void *data, uint16_t len, const linkaddr_t *src, const linkaddr_t *dest) {
+/**** START OF NEIGHBOUR DISCOVERY FROM TASK 1 ****/
+void receive_packet_callback(const void *data, uint16_t len,
+                             const linkaddr_t *src, const linkaddr_t *dest) {
   if (len != sizeof(data_packet)) {
     return;
   }
@@ -125,7 +118,8 @@ void receive_packet_callback(const void *data, uint16_t len, const linkaddr_t *s
   }
 
   if (!has_detected) {
-    printf("%lu DETECT %lu\n", RTIMER_NOW()/RTIMER_SECOND, received_packet_data.src_id);
+    printf("%lu DETECT %lu\n", RTIMER_NOW() / RTIMER_SECOND,
+           received_packet_data.src_id);
     has_detected = true;
   }
   int rssi = (signed short)packetbuf_attr(PACKETBUF_ATTR_RSSI);
@@ -141,53 +135,54 @@ void receive_packet_callback(const void *data, uint16_t len, const linkaddr_t *s
     has_good_link = true;
   }
 
-  memcpy(&data_light_packet.light_readings, light_readings, sizeof(light_readings));
   data_light_packet.timestamp = curr_timestamp;
+  memcpy(&data_light_packet.light_readings, light_readings,
+         sizeof(light_readings));
   data_light_packet.seq++;
   nullnet_buf = (uint8_t *)&data_light_packet;
   nullnet_len = sizeof(data_light_packet);
   NETSTACK_NETWORK.output(&dest_addr);
 
   printf("%ld TRANSFER %ld\n", RTIMER_NOW(), received_packet_data.src_id);
-  // for(int i = 0; i < READING_LENGTH; i++) {
-  //   printf("sending light reading %d\n", data_light_packet.light_readings[i]);
+  // for(int i = 0; i < LIGHT_READING_LEN; i++) {
+  //   printf("sending light reading %d\n",
+  //   data_light_packet.light_readings[i]);
   // }
 
   // reset the light readings
-  for (int i = 0; i < READING_LENGTH; i++) {
+  for (int i = 0; i < LIGHT_READING_LEN; i++) {
     light_readings[i] = LIGHT_DEFAULT;
   }
   light_index = 0;
 
   return;
 }
-// Assimilated from nbr.c
-// Scheduler function for the sender of neighbour discovery packets
+
 char sender_scheduler(struct rtimer *t, void *ptr) {
-
   static uint16_t i = 0;
-
-  static int NumSleep=0;
+  static int num_sleep = 0;
 
   // Begin the protothread
   PT_BEGIN(&pt);
 
   // Get the current time stamp
   curr_timestamp = clock_time();
-  printf("Start clock %lu ticks, timestamp %3lu.%03lu\n", curr_timestamp, curr_timestamp / CLOCK_SECOND,
-         ((curr_timestamp % CLOCK_SECOND)*1000) / CLOCK_SECOND);
+  printf("Start clock %lu ticks, timestamp %3lu.%03lu\n", curr_timestamp,
+         curr_timestamp / CLOCK_SECOND,
+         ((curr_timestamp % CLOCK_SECOND) * 1000) / CLOCK_SECOND);
 
-  while(1){
+  while (1) {
 
     // radio on
     NETSTACK_RADIO.on();
 
     // send NUM_SEND number of neighbour discovery beacon packets
-    for(i = 0; i < NUM_SEND; i++){
+    for (i = 0; i < NUM_SEND; i++) {
 
-      // Initialize the nullnet module with information of packet to be trasnmitted
-      nullnet_buf = (uint8_t *)&data_packet; //data transmitted
-      nullnet_len = sizeof(data_packet); //length of data transmitted
+      // Initialize the nullnet module with information of packet to be
+      // trasnmitted
+      nullnet_buf = (uint8_t *)&data_packet; // data transmitted
+      nullnet_len = sizeof(data_packet);     // length of data transmitted
 
       data_packet.seq++;
 
@@ -195,56 +190,54 @@ char sender_scheduler(struct rtimer *t, void *ptr) {
 
       data_packet.timestamp = curr_timestamp;
 
-      //printf("Send seq# %lu  @ %8lu ticks   %3lu.%03lu\n", data_packet.seq, curr_timestamp, curr_timestamp / CLOCK_SECOND, ((curr_timestamp % CLOCK_SECOND)*1000) / CLOCK_SECOND);
+      NETSTACK_NETWORK.output(&dest_addr); // Send packet
 
-      NETSTACK_NETWORK.output(&dest_addr); //Packet transmission
-
-
-      // wait for WAKE_TIME before sending the next packet
-      if(i != (NUM_SEND - 1)){
-
-        rtimer_set(t, RTIMER_TIME(t) + WAKE_TIME, 1, (rtimer_callback_t)sender_scheduler, ptr);
+      if (i != (NUM_SEND - 1)) {
+        rtimer_set(t, RTIMER_TIME(t) + WAKE_TIME, 1,
+                   (rtimer_callback_t)sender_scheduler, ptr);
         PT_YIELD(&pt);
-
       }
-
     }
 
     // sleep for a random number of slots
-    if(SLEEP_CYCLE != 0){
+    if (SLEEP_CYCLE != 0) {
 
       // radio off
       NETSTACK_RADIO.off();
 
-      NumSleep = 13 - 1; // 7 - 1 on receiver
+      num_sleep = 13 - 1; // Used for DISCO; 7 - 1 on receiver
 
       // NumSleep should be a constant or static int
-      for(i = 0; i < NumSleep; i++){
-        rtimer_set(t, RTIMER_TIME(t) + SLEEP_SLOT, 1, (rtimer_callback_t)sender_scheduler, ptr);
+      for (i = 0; i < num_sleep; i++) {
+        rtimer_set(t, RTIMER_TIME(t) + SLEEP_SLOT, 1,
+                   (rtimer_callback_t)sender_scheduler, ptr);
         PT_YIELD(&pt);
       }
-
     }
   }
 
   PT_END(&pt);
 }
 
-// Modified from nbr.c
 PROCESS_THREAD(nbr_discovery_process, ev, data) {
   PROCESS_BEGIN();
   data_light_packet.src_id = node_id;
   data_light_packet.seq = 0;
+
   data_packet.src_id = node_id;
-  data_packet.device_type = SENDER_TYPE; // 0 for sender, 1 for receiver
+  data_packet.device_type = SENDER_TYPE;
   data_packet.seq = 0;
   nullnet_set_input_callback(receive_packet_callback);
   linkaddr_copy(&dest_addr, &linkaddr_null);
 
   printf("CC2650 neighbour discovery\n");
-  printf("Node %d will be sending packet of size %d Bytes\n", node_id, (int)sizeof(data_packet_struct));
+  printf("Node %d will be sending packet of size %d Bytes\n", node_id,
+         (int)sizeof(data_packet_struct));
 
-  rtimer_set(&rt, RTIMER_NOW() + (RTIMER_SECOND / 1000), 1, (rtimer_callback_t)sender_scheduler, NULL);
+  rtimer_set(&rt, RTIMER_NOW() + (RTIMER_SECOND / 1000), 1,
+             (rtimer_callback_t)sender_scheduler, NULL);
 
   PROCESS_END();
 }
+
+/**** END OF NEIGHBOUR DISCOVERY FROM TASK 1 *****/
